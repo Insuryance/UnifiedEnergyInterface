8b04a79090405a03e8c2a514e8e5f7e0ec7ce731
Suryansham <isitsuryansham@gmail.com>
2025-07-05 19:32:00 +0530

    Update and rename data.json to data.js

---
+++ data.js
@@ -0,0 +1,11 @@
+const data = {
+  gridFrequency: 50.03,
+  demandMW: 195400,
+  carbonIntensity: 605,
+  ueiScores: {
+    Maharashtra: 80,
+    TamilNadu: 75,
+    Gujarat: 78,
+    Karnataka: 82,
+  }
+};
