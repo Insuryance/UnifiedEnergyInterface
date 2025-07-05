1e01e4cc4a11eb0efe8a3235ade03c8e1e6e212a
Suryansham <isitsuryansham@gmail.com>
2025-07-06 00:26:52 +0530

    Update data.js

--- data.js
+++ data.js
@@ -3,9 +3,13 @@ const data = {
   demandMW: 195400,
   carbonIntensity: 605,
   ueiScores: {
+    
     Maharashtra: 80,
+    
     TamilNadu: 75,
+    
     Gujarat: 78,
+    
     Karnataka: 82,
   }
 };
