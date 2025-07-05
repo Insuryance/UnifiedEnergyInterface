d0337e8893903f52590c9e2e102d5a61a6adfd38
Suryansham <isitsuryansham@gmail.com>
2025-07-06 00:28:00 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,15 +1,34 @@
 const data = {
   gridFrequency: 50.03,
   demandMW: 195400,
-  carbonIntensity: 605,
-  ueiScores: {
-    
-    Maharashtra: 80,
-    
-    TamilNadu: 75,
-    
-    Gujarat: 78,
-    
-    Karnataka: 82,
+  carbonIntensity: null, // We’ll calculate this
+  ueiScores: {},
+
+  // Add mock generation source mix
+  energyMix: {
+    Maharashtra: {
+      coal: 60,
+      solar: 20,
+      wind: 10,
+      hydro: 10
+    },
+    TamilNadu: {
+      coal: 40,
+      solar: 30,
+      wind: 25,
+      hydro: 5
+    },
+    Gujarat: {
+      coal: 55,
+      solar: 25,
+      wind: 10,
+      hydro: 10
+    },
+    Karnataka: {
+      coal: 35,
+      solar: 30,
+      wind: 25,
+      hydro: 10
+    }
   }
 };
